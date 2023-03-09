58ea46921ef8d9d0bf1469a0a9869c2eb258cc02
Dove-97 <126733440+Dove-97@users.noreply.github.com>
2023-03-09 13:46:43 +0900

    rty

--- no pro/Assets/BulletController.cs
+++ no pro/Assets/BulletController.cs
@@ -7,6 +7,9 @@ public class BulletController : MonoBehaviour
     // 총알이 날라가는 속도
     private float Speed;
 
+    private int hp;
+
+    public GameObject fxPrefab;
 
     // 총알이 날아가야할 방향
     public Vector3 Direction { get; set; }
@@ -16,6 +19,9 @@ public class BulletController : MonoBehaviour
     {
         // 속도 초기값
         Speed = 10.0f;
+
+        hp = 3;
+
     }
 
 
@@ -29,8 +35,31 @@ public class BulletController : MonoBehaviour
     // 충돌체와 물리엔진이 포함된 오브젝트가 다른 충돌체와 충돌한다면 실행되는 함수.
     private void OnTriggerEnter2D(Collider2D collistion)
     {
-        DestroyObject(this.gameObject);
+
+        --hp;
+
+        GameObject Obj = Instantiate(fxPrefab);
+
+        GameObject camera = new GameObject("Camera Test");
+        camera.AddComponent<CameraController>();
+
+        Obj.transform.position = transform.position;
+
+        DestroyObject(collistion.transform.gameObject);
+
+        if (hp == 0)
+            DestroyObject(collistion.transform.gameObject);
+
     }
 
+    //private void OnTriggerStay2D(Collider2D collision)
+    //{
+    //    print("Stay");
+    //}
+
+    //private void OnTriggerExit2D(Collider2D collision)
+    //{
+    //    print("Exit");
+    //}
 
 }
